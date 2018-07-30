ee3a1ec3fbb3771fbbcf8f3d9a784ee5bfc8b3d9
manibharathi-k <41906206+manibharathi-k@users.noreply.github.com>
2018-07-30 12:44:32 +0000

    Create PoNe.java

---
+++ PoNe.java
@@ -0,0 +1,22 @@
+ import java.util.*;
+class PoNe
+{
+    public static void main(String ars[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>0)
+        {
+            System.out.print("positive");
+        }
+        else if(a==0)
+        {
+            System.out.print("zero");
+        }
+        else
+        {
+             System.out.print("negative");
+        }
+        
+    }
+}
